19992ec192b22dbe9ae374264e236bf52fdd3789
Jongmin Park <9290341+GzuPark@users.noreply.github.com>
2019-09-05 14:54:10 +0900

    Added testcases
    - check URL of dlib landmarks detector
    - compare with results images

---
+++ tests/conftest.py
@@ -0,0 +1,24 @@
+import os
+
+import pytest
+
+
+@pytest.fixture()
+def check_url():
+    url = {}
+    filename = "shape_predictor_68_face_landmarks.dat"
+    url["dlib"] = f"https://raw.githubusercontent.com/davisking/dlib-models/master/{filename}.bz2"
+
+    return url
+
+
+@pytest.fixture()
+def get_args():
+    class Args(object):
+        model = "shape_predictor_68_face_landmarks.dat"
+        image = "example1"
+        landmarks = True
+        webcam = False
+        quaternion = False
+
+    return Args
---
+++ tests/unit/test_puton.py
@@ -0,0 +1,57 @@
+import os
+
+from main import PutOn
+
+
+def test_puton_saved_images(get_args):
+    args = get_args
+
+    puton = PutOn(args)
+    puton.images()
+
+    realpath = os.path.dirname(os.path.realpath(__file__))
+    src_path = os.path.abspath(os.path.join(realpath, "..", "..", "results"))
+    dst_path = os.path.abspath(os.path.join(realpath, "..", "data"))
+
+    sources = []
+    sources.append( os.path.join(src_path, "landmarks_center.jpg") )
+    sources.append( os.path.join(src_path, "puton_center.jpg") )
+
+    targets = []
+    targets.append( os.path.join(dst_path, "landmarks_center.jpg") )
+    targets.append( os.path.join(dst_path, "puton_center.jpg") )
+
+    for src, tar in zip(sources, targets):
+        with open(src, "rb") as img:
+            src_img = img.read()
+        with open(tar, "rb") as img:
+            tar_img = img.read()
+
+        assert src_img == tar_img
+
+
+def test_puton_saved_quaternion_images(get_args):
+    args = get_args
+    args.landmarks = False
+    args.quaternion = True
+
+    puton = PutOn(args)
+    puton.images()
+
+    realpath = os.path.dirname(os.path.realpath(__file__))
+    src_path = os.path.abspath(os.path.join(realpath, "..", "..", "results"))
+    dst_path = os.path.abspath(os.path.join(realpath, "..", "data"))
+
+    sources = []
+    sources.append( os.path.join(src_path, "puton_soft_right.jpg") )
+
+    targets = []
+    targets.append( os.path.join(dst_path, "puton_soft_right.jpg") )
+
+    for src, tar in zip(sources, targets):
+        with open(src, "rb") as img:
+            src_img = img.read()
+        with open(tar, "rb") as img:
+            tar_img = img.read()
+
+        assert src_img == tar_img
